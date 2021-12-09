41991e6fc8c92027f65044d25d8ce9828fded7c6
Beatriz Salles <beatrizsallesss@gmail.com>
2021-12-09 00:01:28 -0300

    :wrench: chore: Add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  theme,
-} from "@chakra-ui/react"
-
-
+import * as React from 'react'
+import { ChakraProvider, Box, Text, theme } from '@chakra-ui/react'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
