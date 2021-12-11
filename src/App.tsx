6564a9033379e22cc998618a87a4152a33704e2c
Beatriz Salles <beatrizsallesss@gmail.com>
2021-12-11 13:48:23 -0300

    :sparkles: feat: Create route page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CustomRoutes } from './routes'
+
 import { theme } from './styles/theme'
-import { Home } from './pages/Home'
+
 import '@fontsource/vt323'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <CustomRoutes />
   </ChakraProvider>
 )
